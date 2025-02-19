8141d7fc109cf6a3643c685ee10c764ed0576fd1
Jules Gossiaux <gossiaux.jules@ind-thuin.be>
2025-02-19 15:55:37 +0100

    bon fonctionnement de auth, de .env, bonne hiérarchie. Prochainement: Créations des leagues

--- src/scripts/supabaseClient.js
+++ src/scripts/supabaseClient.js
@@ -1,27 +1,9 @@
-// import { createClient } from '@supabase/supabase-js'
-// import dotenv from 'dotenv'
+import { createClient } from '@supabase/supabase-js'
 
-// dotenv.config()
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// const supabaseUrl = process.env.SUPABASE_URL
-// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
+export default supabase
 
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-
-// Charger les variables d'environnement
-dotenv.config();
-
-// Vérifier que les variables sont bien définies
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("❌ Les variables d'environnement SUPABASE_URL et SUPABASE_ANON_KEY doivent être définies.");
-}
-
-// Créer le client Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
